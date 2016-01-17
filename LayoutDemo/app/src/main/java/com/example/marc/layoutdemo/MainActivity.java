fc866da1b999f0526c27b2bc33e1917d82374b61
Marc Anderson <marc_anderson_1981@yahoo.com>
2016-01-17 14:38:30 -0600

    several new projects

---
+++ LayoutDemo/app/src/main/java/com/example/marc/layoutdemo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.marc.layoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view){
+        ImageView krusty = (ImageView) findViewById(R.id.krusty);
+        ImageView moe = (ImageView) findViewById(R.id.moe);
+
+        if(krusty.getAlpha() == 1) {
+            krusty.animate().alpha(0f).setDuration(1000);
+            moe.animate().alpha(1f).setDuration(2000);
+        }else if(moe.getAlpha() == 1){
+            moe.animate().alpha(0f).setDuration(1000);
+            krusty.animate().alpha(1f).setDuration(2000);
+        }
+    }
+
+    public void enterNick(View view){
+        ImageView nick = (ImageView) findViewById(R.id.nick);
+        ImageView krusty = (ImageView) findViewById(R.id.krusty);
+        ImageView moe = (ImageView) findViewById(R.id.moe);
+        nick.animate().alpha(1f).setDuration(2000);
+        krusty.animate().alpha(0f).setDuration(500);
+    }
+
+    public void moveNick(View view){
+        ImageView nick = (ImageView) findViewById(R.id.nick);
+        ImageView chief = (ImageView) findViewById(R.id.chief);
+        if(nick.getAlpha() == 1) {
+            nick.animate().translationXBy(1000f).setDuration(3000);
+            chief.animate().translationXBy(1000f).setDuration(3000);
+        }
+    }
+
+    public void chiefSpin(View view){
+        ImageView chief = (ImageView) findViewById(R.id.chief);
+        chief.animate().scaleX(0.5f).scaleY(0.5f).setDuration(4000);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView chief = (ImageView) findViewById(R.id.chief);
+        chief.setTranslationX(-1000f);
+    }
+}
---
+++ NumberShapes/app/src/main/java/com/example/marc/numbershapes/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.marc.numbershapes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    class Number {
+
+        int number;
+
+        public boolean isSquare(){
+
+            double squareRoot = Math.sqrt(number);
+
+            if (squareRoot == Math.floor(squareRoot)) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+
+        public boolean isTriangular(){
+
+            int x = 1;
+
+            int triangularNumber = 1;
+
+            while (triangularNumber < number ) {
+
+                x++;
+
+                triangularNumber = triangularNumber + x;
+
+            }
+
+            if (triangularNumber == number ) {
+
+                return true;
+
+            } else {
+
+                return false;
+
+            }
+
+        }
+
+    }
+
+
+    public void testNumber(View view){
+
+        EditText userNumber = (EditText)findViewById(R.id.userNumber);
+        Log.i("userNumber", userNumber.getText().toString());
+        String message = "";
+
+        if(userNumber.getText().toString().isEmpty()){
+            message = "Please enter a number";
+        } else {
+            Number myNumber = new Number();
+
+            myNumber.number = Integer.parseInt(userNumber.getText().toString());
+
+            if (myNumber.isSquare()) {
+                if (myNumber.isTriangular()) {
+
+                    message = myNumber.number + " is both triangular and square!";
+
+                } else {
+                    message = myNumber.number + " is square, but it is not triangular.";
+                }
+            } else {
+                if (myNumber.isTriangular()) {
+
+                    message = myNumber.number + " is triangular but not square!";
+
+                } else {
+                    message = myNumber.number + " is neither triangular or square.";
+                }
+            }
+        }
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
